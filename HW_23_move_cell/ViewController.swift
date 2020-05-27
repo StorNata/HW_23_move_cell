e2f05b302e3ad40f9d771691bd5f40d706cdabc9
Storozheva Nataliia <storozheva@i.ua>
2020-05-27 07:00:19 +0300

    Initial Commit

---
+++ HW_23_move_cell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW_23_move_cell
+//
+//  Created by Nataliia Storozheva on 27.05.2020.
+//  Copyright © 2020 Nataliia Storozheva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
